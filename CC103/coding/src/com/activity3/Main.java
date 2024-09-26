5f1c164c36177b1a5a316edfdb7dc983a55e9d11
jeiyanet <jeiya5934@proton.me>
2024-09-26 13:15:29 +0800

    feat: add CC103

---
+++ CC103/coding/src/com/activity3/Main.java
@@ -0,0 +1,49 @@
+package com.activity3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*  _          _             
+           / \   _ __ (_) __ _  __ _ 
+          / _ \ | '_ \| |/ _` |/ _` |
+         / ___ \| | | | | (_| | (_| |
+        /_/   \_\_| |_|_|\__,_|\__, |
+                               |___/  */
+
+        // Initialize variables
+        String firstName,midName, lastName;
+        int hours, rate, salary;
+
+        // Create a Scanner Object
+        Scanner scan = new Scanner(System.in);
+
+        // Start user input
+        // NOTE: Design/alignment/words for the screen output may vary depends on your desire. Just make sure that the data needed are properly asked, entered and displayed.
+        System.out.print("Enter First Name \n ~> ");
+        firstName = scan.nextLine();
+
+        System.out.print("Enter Middle Name \n ~> ");
+        midName = scan.nextLine();
+
+        System.out.print("Enter Last Name \n ~> ");
+        lastName = scan.nextLine();
+
+        System.out.print("Enter Number of hours worked \n ~> ");
+        hours = scan.nextInt();
+
+        System.out.print("Enter the rate per hour \n ~> ");
+        rate = scan.nextInt();
+
+        // Caculate the salary
+        salary = hours * rate;
+
+        // Output the information
+        System.out.printf("My name is %s %s %s \n", firstName, midName, 
+        lastName);
+        System.out.printf("My salary is %d \n", salary);
+
+        // Close the scanner
+        scan.close();
+    }
+}
