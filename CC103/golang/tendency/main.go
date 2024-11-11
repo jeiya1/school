faf49a452f833ec89f41b6a5f7b4ed0e874b7426
jeiyanet <jeiya5934@proton.me>
2024-11-11 19:09:44 +0800

    fix: del unused vars in tendency

--- CC103/golang/tendency/main.go
+++ CC103/golang/tendency/main.go
@@ -2,24 +2,6 @@ package main
 
 import "fmt"
 
-var (
-	// Colors and font options via ANSI escape codes
-	Reset     = "\033[0m"
-	Black     = "\033[30m"
-	Red       = "\033[31m"
-	Green     = "\033[32m"
-	Yellow    = "\033[33m"
-	Blue      = "\033[34m"
-	Magenta   = "\033[35m"
-	Cyan      = "\033[36m"
-	Gray      = "\033[37m"
-	White     = "\033[97m"
-	Bold      = "\033[1m"
-	Italic    = "\033[3m"
-	Underline = "\033[4m"
-	Invert    = "\033[7m"
-)
-
 func main() {
 	// LL UL f
 	classMs := []int{}
