ba078beca76975fd852d965453bfe478bbf9e55f
jeiyanet <jeiya5934@proton.me>
2025-01-24 20:51:19 +0800

    fix: moved module-info.java

---
+++ CC104/library/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.comprog.library {
+    exports com.comprog.library.author to com.comprog.library.main;
+    exports com.comprog.library.id to com.comprog.library.main;
+    exports com.comprog.library.title to com.comprog.library.main;
+}
--- CC104/library/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module com.comprog.library {
-    exports com.comprog.library.author to comp.comprog.library.main;
-    exports com.comprog.library.id to comp.comprog.library.main;
-    exports com.comprog.library.title to comp.comprog.library.main;
-}
